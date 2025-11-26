fbe1da6db6c61c80bc0465b7d57888dc1f80c560
Patrick Elmer <patrick@elmer.ws>
2025-11-26 23:55:54 +0100

    Convert to super calls

--- dotdict3.py
+++ dotdict3.py
@@ -8,12 +8,11 @@ class DotDict(dict):
             self[k] = v
 
     def __setitem__(self, k, v):
-        if isinstance(v, dict):
-            return dict.__setitem__(self, k, DotDict(v))
-        elif isinstance(v, (list, tuple)):
-            return dict.__setitem__(self, k, DotList(v))
-        else:
-            return dict.__setitem__(self, k, v)
+        if isinstance(v, dict) and not isinstance(v, DotDict):
+            return super().__setitem__(k, DotDict(v))
+        elif isinstance(v, (tuple, list, set, range)):
+            return super().__setitem__(k, DotList(v))
+        return super().__setitem__(k, v)
 
 
 class DotList(list):
